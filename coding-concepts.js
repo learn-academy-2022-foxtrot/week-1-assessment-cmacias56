96ab827abd21ebcf69e937ace36015092645edc2
c.r.macias56@hotmail.com <c.r.macias56@hotmail.com>
2022-09-10 00:40:41 -0700

    2/3 done

--- coding-concepts.js
+++ coding-concepts.js
@@ -6,54 +6,54 @@
 // Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
 
 // --------------------INSTRUCTOR EXAMPLE: What will this log?
-const colors = ["tangerine", "magenta", "lilac", "daffodil"]
-// console.log(colors.push("indigo"))
+//const colors = ["tangerine", "magenta", "lilac", "daffodil"]
+ //console.log(colors.push("indigo"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: value of 5 will produce 
+// b) Verify and explain: value of 5 confirmed, .push added a 5th name/color to the array 
 
 
 // --------------------1) What will this log?
 
-const cohort = "LEARN 2022"
-// console.log(cohort.length)
+//const cohort = "LEARN 2022"
+ //console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: value of 10 will produce
+// b) Verify and explain: 10 is correct, .length will give the amount of every charater including space
 
 
 // --------------------2) What will this log?
 
-const greeting = "Hello World!"
+//const greeting = "Hello World!"
 // console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Hello World will produce 4 times
+// b) Verify and explain: the letter o produced, the 4 indicated the o in hello based on 0-4 array. H-0 E-1 L-2 L-3 O-4
 
 
 // --------------------3) What will this log?
 
-const languages = ["JavaScript", "Ruby", "Python", "C++"]
-const index = 1
+//const languages = ["JavaScript", "Ruby", "Python", "C++"]
+//const index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: ruby will produce
+// b) Verify and explain: ruby was produced, elements list from 0,1,2,3. Javascript - 0, ruby -1, python - 2, c++ - 3
 
 
 // --------------------4) What will this log?
 
-const weekendDays = ["saturday", "sunday"]
+//const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: toUppercase is not a variable and does not have a function. 
+// b) Verify and explain: correct. code did not provide any function to toUpperCase, it is not a variable. 
 
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+ console.log(typeof dataTypes.length)
 
-// a) Your answer:
+// a) Your answer: 
 // b) Verify and explain:
